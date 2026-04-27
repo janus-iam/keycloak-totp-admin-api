8a3e8a10daf23e457c350143b76b324513781106
newtondotcom <asphalt9fr@gmail.com>
2026-04-27 12:11:42 +0200

    fix: specify `MediaType` for `ApiException`

--- src/main/java/com/janus/keycloak/totpadmin/ApiException.java
+++ src/main/java/com/janus/keycloak/totpadmin/ApiException.java
@@ -2,11 +2,15 @@ package com.janus.keycloak.totpadmin;
 
 import com.janus.keycloak.totpadmin.dto.MessageResponse;
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 public class ApiException extends WebApplicationException {
 
     public ApiException(Response.Status status, String message) {
-        super(Response.status(status).entity(new MessageResponse(message)).build());
+        super(Response.status(status)
+            .type(MediaType.APPLICATION_JSON)
+            .entity(new MessageResponse(message))
+            .build());
     }
 }
